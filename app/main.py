69c2df17cafb18e28c2849b3c6449ae20b8d26e7
Melezhyk Oleksandr <melezhyk02@gmail.com>
2026-01-21 19:36:21 +0200

    implement py-herbivore-and-carnivore

--- app/main.py
+++ app/main.py
@@ -1,1 +1,36 @@
-# write your code here
+class Animal:
+
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        if health <= 0:
+            raise ValueError("Health is too low")
+        self.name = name
+        self.health = health
+        Animal.alive.append(self)
+        self.hidden = False
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        if self.hidden:
+            self.hidden = False
+        else:
+            self.hidden = True
+
+
+class Carnivore(Animal):
+
+    @staticmethod
+    def bite(other_herbivore: Herbivore) -> None:
+        if (isinstance(other_herbivore, Herbivore)
+                and other_herbivore.hidden is False):
+            other_herbivore.health -= 50
+            if other_herbivore.health <= 0:
+                Animal.alive.remove(other_herbivore)
